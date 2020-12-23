7bddf83ecb68eca87b2284459fb99ca7fcc5c905
aekgmla77 <aekgmla@naver.com>
2020-12-23 13:55:24 +0900

    cdf

---
+++ js/function2.js
@@ -0,0 +1,37 @@
+// function nameFunc() {
+let name1 = window.prompt('숫자를 입력하세요.');
+// let name2 = window.prompt('숫자를 입력하세요.');
+// let name1 = document.getElementById('name').value;
+
+
+diffSum(name1, name2); // 입력받은 두수 사이의 합: 1~5
+let numAry = [];
+
+for (let i = 0; i < 5; i++) {
+    let name1 = window.prompt('숫자를 입력하세요.');
+    numAry[i] = parseInt(name1);
+}
+document.write(numAry[i]);
+
+
+// sum(name1, name2);
+// }
+// function sum(a, b) {
+//     let n1 = parseInt(a);
+//     let n2 = parseInt(b);
+//     // 문자열 타입의 숫자 '30'  >>  30으로 바꿔줌
+//     console.log(n1 + n2);
+// }
+// checkGrade(name1); //90:A, 80:B, 70:C, 그외:D
+// function checkGrade(grade) {
+//     if (grade >= 90) {
+//         document.write("A 입니다.");
+//     } else if (grade >= 80) {
+//         document.write("B 입니다.");
+//     } else if (grade >= 70) {
+//         document.write("C 입니다.");
+//     } else {
+//         document.write("D 입니다.");
+//     }
+// }
+
--- js/list.js
+++ js/list.js
@@ -1,21 +1,35 @@
 function myFunc() {
     document.write("<ul><li>Apple</li><li>Banana</li></ul>");
-
-    //     document.write('<ul>');
-    //     for (let i = 1; i <= 10; i++) {
-    //         // document.write('<p>i의 값은: <b>' + i + '</b>입니다.</p>');
-    //         if (i % 2 == 1) {
-    //             document.write('<li class = "even">' + dan + ' * ' + i + ' = <b>' + (dan * i) + '</li></b>');
-    //         } else {
-    //             document.write('<li class = "odd" >' + dan + ' * ' + i + ' = <b>' + (dan * i) + '</li></b>');
-    //         }
-    //     }
-    //     document.write('</ul>');
-    // }
+    document.write('<ul>');
     let dan = 4;
-    document.write('<table border="1">');
     for (let i = 1; i <= 10; i++) {
-        document.write('<tr><td>' + dan  + '</td><td> * </td><td>' + i + ' </td><td> = </td><td>' + (dan * i) + '</td></tr>');
+        // document.write('<p>i의 값은: <b>' + i + '</b>입니다.</p>');
+        if (i % 2 == 1) {
+            document.write('<li class = "even">' + dan + ' * ' + i + ' = <b>' + (dan * i) + '</li></b>');
+        } else {
+            document.write('<li class = "odd" >' + dan + ' * ' + i + ' = <b>' + (dan * i) + '</li></b>');
+        }
     }
-    document.write('</table>');
+    document.write('</ul>');
 }
+    // let dan = 4;
+    // document.write('<table border="1" width = 10%>');
+    // for (let i = 1; i <= 9; i++) {
+    //     document.write('<tr align="center">');
+    //     document.write('<td>' + dan  + ' * ' + i + ' </td><td> = </td><td>' + (dan * i) + '</td>');
+    //     document.write('</tr>');
+    // }
+    // document.write('</table>');
+    // let dan = 4;
+    // document.write('<table border="1" width = 10%>');
+    // for (let i = 1; i <= 9; i++) {
+    //     document.write('<tr align="center">');
+    //     if (i % 2 == 1) {
+    //         document.write('<td class = "even">' + dan + ' * ' + i + ' </td><td> = </td><td>' + (dan * i) + '</td>');
+    //     } else {
+    //         document.write('<td class = "odd">' + dan + ' * ' + i + ' </td><td> = </td><td>' + (dan * i) + '</td>');
+    //     }
+    //     document.write('</tr>');
+    // }
+    // document.write('</table>');
+// }
