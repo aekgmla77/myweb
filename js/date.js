b768bf953d3a402262210f5dcd4e68e5eff4817c
aekgmla77 <aekgmla@naver.com>
2021-01-03 21:00:47 +0900

    sgd

--- js/date.js
+++ js/date.js
@@ -22,17 +22,24 @@ function showCal(yyyy, mm) {
         th.innerHTML = day;
         tr.append(th);
     }
+    let cnt = 0;
     table.append(tr);
     let dates = [];
-    let first = new Date(yyyy + '-' + mm , -1);
-    let today = new Date(yyyy + '-' + mm);
+    let first = new Date(1900, 01, 01);
+    let today = new Date(yyyy + '-' + mm + -1);
     today= new Date(yyyy, mm, 0);
     console.log(first);
     for (let i = 1; i <= today.getDate(); i++) {
+        for(let n = first.getDate(); n <= i; n++) {
         if (i % 7 == 1) {
             tr = document.createElement('tr');
+        } else if (n % 7 == 0){
+            dates.unshift('*');
         }
+    }
+        
         dates.push[i];
+        dates.unshift[i];
         let td = document.createElement('td');
         td.innerHTML = i;
         tr.append(td);
@@ -42,4 +49,4 @@ function showCal(yyyy, mm) {
     console.info(table);
 }
 
-showCal(2020, 6);
+showCal(1900, 4);
